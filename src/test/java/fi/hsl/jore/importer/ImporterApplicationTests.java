17489e68bc14a6d0b4a0aeca3190c8a26afba459
Lasse Maatta <lasse.maatta@vincit.fi>
2021-03-04 11:18:22 +0200

    Initial import from spring initializr

---
+++ src/test/java/fi/hsl/jore/importer/ImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.hsl.jore.importer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
