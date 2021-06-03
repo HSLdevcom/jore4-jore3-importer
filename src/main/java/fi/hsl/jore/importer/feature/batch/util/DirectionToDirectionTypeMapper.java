892df031c09746dbc5f25c4053a1c4be1f38771d
Lasse Maatta <lasse.maatta@vincit.fi>
2021-06-03 10:37:26 +0300

    Add enumeration and mapping for jore 3 directions
    - Current mapping is a naive algorithm which
      maps "1" as outbound and "2" as inbound

---
+++ src/main/java/fi/hsl/jore/importer/feature/batch/util/DirectionToDirectionTypeMapper.java
@@ -0,0 +1,22 @@
+package fi.hsl.jore.importer.feature.batch.util;
+
+import fi.hsl.jore.importer.feature.jore3.enumerated.Direction;
+import fi.hsl.jore.importer.feature.network.direction_type.field.DirectionType;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.Map;
+
+public final class DirectionToDirectionTypeMapper {
+
+    public static final Map<Direction, DirectionType> TO_DIRECTION_TYPE =
+            HashMap.of(Direction.DIRECTION_1, DirectionType.OUTBOUND,
+                       Direction.DIRECTION_2, DirectionType.INBOUND,
+                       Direction.UNKNOWN, DirectionType.UNKNOWN);
+
+    private DirectionToDirectionTypeMapper() {
+    }
+
+    public static DirectionType resolveDirectionType(final Direction direction) {
+        return TO_DIRECTION_TYPE.getOrElse(direction,
+                                           DirectionType.UNKNOWN);
+    }
+}
---
+++ src/main/java/fi/hsl/jore/importer/feature/jore3/enumerated/Direction.java
@@ -0,0 +1,37 @@
+package fi.hsl.jore.importer.feature.jore3.enumerated;
+
+import fi.hsl.jore.importer.feature.jore3.mapping.JoreEnumeration;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@JoreEnumeration(name = "Suunta")
+public enum Direction {
+    DIRECTION_1(1, "Suunta 1"),
+    DIRECTION_2(2, "Suunta 2"),
+
+    UNKNOWN(-9999, "Unknown direction"),
+    ;
+
+    private final int value;
+    private final String description;
+
+    Direction(final int value, final String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int value() {
+        return value;
+    }
+
+    public static Optional<Direction> of(final int value) {
+        return Arrays.stream(values())
+                     .filter(direction -> direction.value == value)
+                     .findFirst();
+    }
+}
