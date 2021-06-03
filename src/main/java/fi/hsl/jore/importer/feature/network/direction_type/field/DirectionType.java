6bf2ef3cbea604cdd42c5b4b7727be232ec0c713
Lasse Maatta <lasse.maatta@vincit.fi>
2021-06-03 10:37:26 +0300

    Add enumeration and mapping for jore 3 directions
    - Current mapping is a naive algorithm which
      maps "1" as outbound and "2" as inbound

---
+++ src/main/java/fi/hsl/jore/importer/feature/network/direction_type/field/DirectionType.java
@@ -0,0 +1,29 @@
+package fi.hsl.jore.importer.feature.network.direction_type.field;
+
+import java.util.Arrays;
+
+public enum DirectionType {
+    INBOUND("inbound"),
+    OUTBOUND("outbound"),
+    CLOCKWISE("clockwise"),
+    ANTICLOCKWISE("anticlockwise"),
+    UNKNOWN("unknown"),
+    ;
+
+    private final String label;
+
+    DirectionType(final String label) {
+        this.label = label;
+    }
+
+    public String label() {
+        return label;
+    }
+
+    public static DirectionType of(final String val) {
+        return Arrays.stream(values())
+                     .filter(type -> type.label.equalsIgnoreCase(val))
+                     .findFirst()
+                     .orElseThrow();
+    }
+}
