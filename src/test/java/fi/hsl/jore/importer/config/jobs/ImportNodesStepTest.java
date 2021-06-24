3030d57a017ce56dae2c97611e38ac5ece44f53f
Lasse Maatta <lasse.maatta@vincit.fi>
2021-06-24 10:34:09 +0300

    Added more test data to nodes and links

--- src/test/java/fi/hsl/jore/importer/config/jobs/ImportNodesStepTest.java
+++ src/test/java/fi/hsl/jore/importer/config/jobs/ImportNodesStepTest.java
@@ -6,13 +6,19 @@ import fi.hsl.jore.importer.feature.infrastructure.node.dto.Node;
 import fi.hsl.jore.importer.feature.infrastructure.node.dto.NodeType;
 import fi.hsl.jore.importer.feature.infrastructure.node.repository.INodeTestRepository;
 import fi.hsl.jore.importer.util.GeometryUtil;
+import io.vavr.Tuple;
+import io.vavr.Tuple4;
 import io.vavr.collection.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Point;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
+
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -30,35 +36,59 @@ public class ImportNodesStepTest extends BatchIntegrationTest {
                                                       "importNodesStep",
                                                       "commitNodesStep");
 
+    // External ID of the node,
+    // The node location
+    // The node projected location,
+    // and the node type
+    private static final List<Tuple4<ExternalId, Point, Optional<Point>, NodeType>> NODES = List.of(
+            Tuple.of(ExternalId.of("c"),
+                     GeometryUtil.toPoint(GeometryUtil.SRID_WGS84, new Coordinate(6, 5)),
+                     Optional.of(GeometryUtil.toPoint(GeometryUtil.SRID_WGS84, new Coordinate(13, 12))),
+                     NodeType.STOP),
+            Tuple.of(ExternalId.of("d"),
+                     GeometryUtil.toPoint(GeometryUtil.SRID_WGS84, new Coordinate(17, 16)),
+                     Optional.empty(),
+                     NodeType.CROSSROADS),
+            Tuple.of(ExternalId.of("e"),
+                     GeometryUtil.toPoint(GeometryUtil.SRID_WGS84, new Coordinate(19, 18)),
+                     Optional.empty(),
+                     NodeType.CROSSROADS),
+            Tuple.of(ExternalId.of("f"),
+                     GeometryUtil.toPoint(GeometryUtil.SRID_WGS84, new Coordinate(8, 7)),
+                     Optional.of(GeometryUtil.toPoint(GeometryUtil.SRID_WGS84, new Coordinate(21, 20))),
+                     NodeType.STOP)
+    );
+
     @Autowired
     private INodeTestRepository nodeRepository;
 
-    @Test
-    public void givenSampleDataInSourceDatabase_andAnEmptyTargetDatabase_whenImportNodeStepsAreRun_thenNodesCAndDAreImported() {
+    @BeforeEach
+    public void setup() {
         assertThat(nodeRepository.empty(),
                    is(true));
+    }
 
+    @Test
+    public void givenSampleDataInSourceDatabase_andAnEmptyTargetDatabase_whenImportNodeStepsAreRun_thenNodesCAndDAreImported() {
         runSteps(STEPS);
 
         assertThat(nodeRepository.count(),
-                   is(2));
+                   is(NODES.size()));
 
-        final Node nodeC = nodeRepository.findByExternalId(ExternalId.of("c"))
-                                         .orElseThrow();
+        NODES.forEach(expectedNodeParams -> {
+            final ExternalId externalId = expectedNodeParams._1;
+            final Node node = nodeRepository.findByExternalId(externalId)
+                                            .orElseThrow();
 
-        assertThat(nodeC.location(),
-                   is(GeometryUtil.toPoint(GeometryUtil.SRID_WGS84,
-                                           new Coordinate(13, 12))));
-        assertThat(nodeC.nodeType(),
-                   is(NodeType.CROSSROADS));
-
-        final Node nodeD = nodeRepository.findByExternalId(ExternalId.of("d"))
-                                         .orElseThrow();
-
-        assertThat(nodeD.location(),
-                   is(GeometryUtil.toPoint(GeometryUtil.SRID_WGS84,
-                                           new Coordinate(17, 16))));
-        assertThat(nodeD.nodeType(),
-                   is(NodeType.CROSSROADS));
+            assertThat(String.format("node %s should have correct location", externalId),
+                       node.location(),
+                       is(expectedNodeParams._2));
+            assertThat(String.format("node %s should have correct projected location", externalId),
+                       node.projectedLocation(),
+                       is(expectedNodeParams._3));
+            assertThat(String.format("node %s should have correct node type", externalId),
+                       node.nodeType(),
+                       is(expectedNodeParams._4));
+        });
     }
 }
--- src/test/resources/sql/source/populate_nodes.sql
+++ src/test/resources/sql/source/populate_nodes.sql
@@ -2,5 +2,7 @@ INSERT INTO jr_solmu
     (soltunnus, soltyyppi, solomx, solomy, solstmx, solstmy)
 VALUES ('a', 'X', null, null, null, null),
        ('b', 'X', null, null, 10, 11),
-       ('c', 'X', 12, 13, 1, 1),
-       ('d', 'X', 16, 17, 1, 1);
+       ('c', 'P', 5, 6, 12, 13),
+       ('d', 'X', 16, 17, 1, 1),
+       ('e', 'X', 18, 19, 1, 1),
+       ('f', 'P', 7, 8, 20, 21);
