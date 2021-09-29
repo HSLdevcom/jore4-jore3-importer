7be639b3be1fde0ccb04795e22d0c3e84e37fdb4
Petri Kainulainen <petri.kainulainen@vincit.fi>
2021-09-29 13:22:30 +0300

    Write tests for RouteStopPointImportRepository

---
+++ src/test/resources/sql/destination/populate_route_stop_points.sql
@@ -0,0 +1,14 @@
+INSERT INTO network.network_route_stop_points (
+        network_route_point_id,
+        network_route_stop_point_ext_id,
+        network_route_stop_point_order,
+        network_route_stop_point_hastus_point,
+        network_route_stop_point_timetable_column
+)
+VALUES (
+        '00000cc9-d691-492e-b55c-294b903fca33',
+        '1234528-1113227',
+        1,
+        false,
+        1
+);
---
+++ src/test/resources/sql/destination/populate_route_stop_points_staging.sql
@@ -0,0 +1,12 @@
+INSERT INTO network.network_route_stop_points_staging (
+        network_route_stop_point_ext_id,
+        network_route_stop_point_order,
+        network_route_stop_point_hastus_point,
+        network_route_stop_point_timetable_column
+)
+VALUES (
+        '1234528-1113227',
+        6,
+        true,
+        12
+);
