d627f34a95ceae413a150c635d6f8ed9afc5d760
Petri Kainulainen <petri.kainulainen@vincit.fi>
2021-09-30 14:58:56 +0300

    Write tests for RouteLinkImportRepository

---
+++ src/test/resources/sql/destination/populate_route_links_staging.sql
@@ -0,0 +1,12 @@
+INSERT INTO network.network_route_links_staging (
+        network_route_link_ext_id,
+        network_route_direction_ext_id,
+        infrastructure_link_ext_id,
+        network_route_link_order
+)
+VALUES (
+        '1111111',
+        '1001-2-20211004',
+        '1-9410205-9410203',
+        10
+);
