f5d39fb026d2191a6f3c4a210ec2914f767cc40f
Petri Kainulainen <petri.kainulainen@vincit.fi>
2021-10-08 13:00:48 +0300

    Remove unnecessary cast

--- src/test/resources/sql/destination/populate_infrastructure_links.sql
+++ src/test/resources/sql/destination/populate_infrastructure_links.sql
@@ -9,7 +9,7 @@ INSERT INTO infrastructure_network.infrastructure_links (
 VALUES (
         '00018613-e3a1-41be-9ba0-a95f61d70aec',
         '1-9410205-9410203',
-        ST_MakeLine(ST_Force3D(point(24.457948, 60.088725)::geometry), ST_Force3D(point(24.468175, 60.15286)::geometry))::geometry,
+        ST_MakeLine(ST_Force3D(point(24.457948, 60.088725)::geometry), ST_Force3D(point(24.468175, 60.15286)::geometry)),
         'road',
         '00002c7a-bd85-43ed-afb9-389b498aaa06',
         '0009b559-0e7c-4549-99eb-5391727d1016'
