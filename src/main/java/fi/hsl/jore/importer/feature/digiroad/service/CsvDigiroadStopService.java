9e8ef36d31df3eaf36724af27144d2e26bda45cc
Petri Kainulainen <petri.kainulainen@vincit.fi>
2021-11-01 13:17:03 +0200

    Fixed review comments

--- src/main/java/fi/hsl/jore/importer/feature/digiroad/service/CsvDigiroadStopService.java
+++ src/main/java/fi/hsl/jore/importer/feature/digiroad/service/CsvDigiroadStopService.java
@@ -1,6 +1,8 @@
 package fi.hsl.jore.importer.feature.digiroad.service;
 
 import fi.hsl.jore.importer.feature.digiroad.entity.DigiroadStop;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +13,6 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -21,23 +21,21 @@ public class CsvDigiroadStopService implements DigiroadStopService {
     private static final Logger LOGGER = LoggerFactory.getLogger(CsvDigiroadStopService.class);
 
     private final Resource csvResource;
-    private final Map<String, DigiroadStop> digiroadStops;
+
+    //This cannot be final because the io.vavr.collection.HashMap is immutable
+    //and .put() method returns a new map which contains the added value.
+    private Map<Integer, DigiroadStop> digiroadStops;
 
     @Autowired
     public CsvDigiroadStopService(@Value("digiroad.stop.csv.file.path") final Resource csvResource) {
         this.csvResource = csvResource;
-        this.digiroadStops = new HashMap<>();
+        this.digiroadStops = HashMap.empty();
     }
 
     @Override
-    public Optional<DigiroadStop> findByElyNumber(String elyNumber) {
-        DigiroadStop stop = digiroadStops.get(elyNumber);
-        if (stop == null) {
-            return Optional.empty();
-        }
-        else {
-            return Optional.of(stop);
-        }
+    public Optional<DigiroadStop> findByNationalId(final int nationalId) {
+        final DigiroadStop stop = digiroadStops.get(nationalId).getOrNull();
+        return Optional.ofNullable(stop);
     }
 
     @PostConstruct
@@ -50,15 +48,14 @@ public class CsvDigiroadStopService implements DigiroadStopService {
             return;
         }
 
-
         try (BufferedReader reader = new BufferedReader(new FileReader(csvResource.getFile()))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                Optional<DigiroadStop> stopContainer = DigiroadStopFactory.fromCsvLine(line);
+                final Optional<DigiroadStop> stopContainer = DigiroadStopFactory.fromCsvLine(line);
 
                 if (stopContainer.isPresent()) {
-                    DigiroadStop stop = stopContainer.get();
-                    digiroadStops.put(stop.elyNumber(), stop);
+                    final DigiroadStop stop = stopContainer.get();
+                    digiroadStops = digiroadStops.put(stop.nationalId(), stop);
                 }
                 else {
                     LOGGER.error("Cannot parse the information of a Digiroad stop from the line: {}", line);
