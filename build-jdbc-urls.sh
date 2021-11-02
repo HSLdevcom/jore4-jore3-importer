e4b300733288c65b57fe687d623e3473a3dc7a54
Petri Kainulainen <petri.kainulainen@vincit.fi>
2021-11-02 10:04:27 +0200

    Fix review comments
    - Update documentation (change the names of configuration properties,
      environment variables, and secrets).
    - Use correct environment variables in the docker-compile.yml file.
    - Use correct environment variables when building JDBC urls.

--- build-jdbc-urls.sh
+++ build-jdbc-urls.sh
@@ -9,8 +9,8 @@ if [ -z "${SOURCE_DB_URL+x}" ]; then
   export SOURCE_DB_URL="jdbc:sqlserver://${SOURCE_DB_HOSTNAME}:${SOURCE_DB_PORT};database=${SOURCE_DB_DATABASE};applicationIntent=ReadOnly"
 fi
 
-# if DESTINATION_DB_URL was not set, build it from parts
-if [ -z "${DESTINATION_DB_URL+x}" ]; then
-  export DESTINATION_DB_PORT=${DESTINATION_DB_PORT:-"5432"}
-  export DESTINATION_DB_URL="jdbc:postgresql://${DESTINATION_DB_HOSTNAME}:${DESTINATION_DB_PORT}/${DESTINATION_DB_DATABASE}?stringtype=unspecified"
+# if IMPORTER_DB_URL was not set, build it from parts
+if [ -z "${IMPORTER_DB_URL+x}" ]; then
+  export IMPORTER_DB_PORT=${IMPORTER_DB_PORT:-"5432"}
+  export IMPORTER_DB_URL="jdbc:postgresql://${IMPORTER_DB_HOSTNAME}:${IMPORTER_DB_PORT}/${IMPORTER_DB_DATABASE}?stringtype=unspecified"
 fi
