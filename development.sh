0b505a32f967326865f100ed54154451f4ee49f7
Tamas Pallos <tamas.pallos@vincit.fi>
2021-11-03 10:14:55 +0200

    docker-compose bundle

--- development.sh
+++ development.sh
@@ -25,6 +25,30 @@ instruct_and_exit() {
   exit 1
 }
 
+download_docker_bundle() {
+  # initialize package folder
+  mkdir -p ./docker
+
+  # compare versions
+  GITHUB_VERSION=$(curl -L https://github.com/HSLdevcom/jore4-flux/releases/download/e2e-docker-compose/RELEASE_VERSION.txt --silent)
+  LOCAL_VERSION=$(cat ./docker/RELEASE_VERSION.txt || echo "unknown")
+
+  # download latest version of the docker-compose package in case it has changed
+  if [ "$GITHUB_VERSION" != "$LOCAL_VERSION" ]; then
+    echo "E2E docker-compose package is not up to date, downloading a new version."
+    curl -L https://github.com/HSLdevcom/jore4-flux/releases/download/e2e-docker-compose/e2e-docker-compose.tar.gz --silent | tar -xzf - -C ./docker/
+  else
+    echo "E2E docker-compose package is up to date, no need to download new version."
+  fi
+
+  # start up all services (and build on-demand)
+  # docker-compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.custom.yml up --build jore4-testdb jore4-hasura jore4-mssqltestdb
+  docker-compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.custom.yml up --build jore4-hasura jore4-testdb
+
+  # start up only some services (and build on-demand)
+  # docker-compose -f ./docker/docker-compose.yml up --build jore4-ui jore4-proxy
+}
+
 start_all() {
   docker-compose up --build -d
 }
@@ -41,6 +65,11 @@ generate_jooq() {
 
 if [[ -z ${COMMAND} ]]; then
   instruct_and_exit
+fi
+
+if [[ ${COMMAND} == "bundle" ]]; then
+  download_docker_bundle
+  exit 0
 fi
 
 if [[ ${COMMAND} == "start" ]]; then
