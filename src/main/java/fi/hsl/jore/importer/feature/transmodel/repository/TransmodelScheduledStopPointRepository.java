45b124c0d84888521590004f9de9000ed927b222
Petri Kainulainen <petri.kainulainen@vincit.fi>
2021-11-17 15:27:20 +0200

    Insert scheduled stop points into Jore 4 db

---
+++ src/main/java/fi/hsl/jore/importer/feature/transmodel/repository/TransmodelScheduledStopPointRepository.java
@@ -0,0 +1,50 @@
+package fi.hsl.jore.importer.feature.transmodel.repository;
+
+import fi.hsl.jore.importer.feature.transmodel.entity.TransmodelScheduledStopPoint;
+import org.jooq.BatchBindStep;
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import static fi.hsl.jore.jore4.jooq.infrastructure_network.Tables.INFRASTRUCTURE_LINK;
+import static fi.hsl.jore.jore4.jooq.internal_service_pattern.tables.ScheduledStopPoint.SCHEDULED_STOP_POINT;
+
+@Repository
+public class TransmodelScheduledStopPointRepository implements ITransmodelScheduledStopPointRepository {
+
+    private final DSLContext db;
+
+    @Autowired
+    public TransmodelScheduledStopPointRepository(@Qualifier("jore4Dsl") final DSLContext db) {
+        this.db = db;
+    }
+
+    @Override
+    public void insert(final List<? extends TransmodelScheduledStopPoint> stopPoints) {
+        final BatchBindStep batch = db.batch(db.insertInto(
+                SCHEDULED_STOP_POINT,
+                SCHEDULED_STOP_POINT.DIRECTION,
+                SCHEDULED_STOP_POINT.LABEL,
+                SCHEDULED_STOP_POINT.LOCATED_ON_INFRASTRUCTURE_LINK_ID,
+                SCHEDULED_STOP_POINT.MEASURED_LOCATION
+        )
+                        .values((String) null, null, null, null)
+        );
+
+        stopPoints.forEach(stopPoint -> batch.bind(
+                stopPoint.directionOnInfraLink().getValue(),
+                stopPoint.label(),
+                db.select(INFRASTRUCTURE_LINK.INFRASTRUCTURE_LINK_ID)
+                        .from(INFRASTRUCTURE_LINK)
+                        .where(INFRASTRUCTURE_LINK.EXTERNAL_LINK_ID.eq(stopPoint.externalInfrastructureLinkId()))
+                        .fetchOneInto(UUID.class),
+                stopPoint.measuredLocation()
+        ));
+
+        batch.execute();
+    }
+}
---
+++ src/test/resources/sql/transmodel/drop_tables.sql
@@ -0,0 +1,2 @@
+TRUNCATE TABLE infrastructure_network.infrastructure_link CASCADE;
+TRUNCATE TABLE internal_service_pattern.scheduled_stop_point CASCADE;
