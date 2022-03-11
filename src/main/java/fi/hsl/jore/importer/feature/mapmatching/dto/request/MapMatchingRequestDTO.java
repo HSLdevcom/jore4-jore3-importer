a95a2f992fd17aeba00e43969c929b90bf62340e
Petri Kainulainen <petri.kainulainen@vincit.fi>
2022-03-11 10:32:01 +0200

    Export route geometries to the Jore 4 database
    - Read route points and geometries from the importer's database.
    - Transform the source information into geojson.
    - Get the Digiroad route geometries from the Map Matching API.
    - Write the Digiroad route geometries to the Jore 4 database.
    - Write tests.
    
    Resolves /HSLdevcom/jore4/issues/617

---
+++ src/main/java/fi/hsl/jore/importer/feature/mapmatching/dto/request/MapMatchingRequestDTO.java
@@ -0,0 +1,42 @@
+package fi.hsl.jore.importer.feature.mapmatching.dto.request;
+
+import org.geojson.LineString;
+
+import java.util.List;
+
+/**
+ * Contains the information that's is send to the
+ * Map Matching API.
+ */
+public class MapMatchingRequestDTO {
+
+    private String routeId;
+    private LineString routeGeometry;
+    private List<RoutePointRequestDTO> routePoints;
+
+    public MapMatchingRequestDTO() {}
+
+    public String getRouteId() {
+        return routeId;
+    }
+
+    public LineString getRouteGeometry() {
+        return routeGeometry;
+    }
+
+    public List<RoutePointRequestDTO> getRoutePoints() {
+        return routePoints;
+    }
+
+    public void setRouteId(final String routeId) {
+        this.routeId = routeId;
+    }
+
+    public void setRouteGeometry(final LineString routeGeometry) {
+        this.routeGeometry = routeGeometry;
+    }
+
+    public void setRoutePoints(final List<RoutePointRequestDTO> routePoints) {
+        this.routePoints = routePoints;
+    }
+}
