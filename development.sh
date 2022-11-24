5a138850a45d2f53799b774412df550df6a6f99b
Jörg Schummer <jorg.schummer@vincit.fi>
2022-11-24 15:39:39 +0200

    Update E2E docker-compose download script for private flux repo
    The github gh-tool is used to download flux-releases instead of a direct https-download.
    The CI jobs also are refactored to use setup-e2e-action instead of using the local development.sh script.
    Also removed the test-docker-compose CI job as it does not give any additional value anymore.

--- development.sh
+++ development.sh
@@ -34,20 +34,24 @@ instruct_and_exit() {
 }
 
 download_docker_bundle() {
-  # initialize package folder
-  mkdir -p ./docker
+  # based on https://github.com/HSLdevcom/jore4-flux#docker-compose
 
-  # compare versions
-  GITHUB_VERSION=$(curl -L https://github.com/HSLdevcom/jore4-flux/releases/download/e2e-docker-compose/RELEASE_VERSION.txt --silent)
-  LOCAL_VERSION=$(cat ./docker/RELEASE_VERSION.txt || echo "unknown")
-
-  # download latest version of the docker-compose package in case it has changed
-  if [ "$GITHUB_VERSION" != "$LOCAL_VERSION" ]; then
-    echo "E2E docker-compose package is not up to date, downloading a new version."
-    curl -L https://github.com/HSLdevcom/jore4-flux/releases/download/e2e-docker-compose/e2e-docker-compose.tar.gz --silent | tar -xzf - -C ./docker/
-  else
-    echo "E2E docker-compose package is up to date, no need to download new version."
+  if ! command -v gh; then
+    echo "Please install the github gh tool on your machine."
+    exit 1
   fi
+
+  # initialize package folder
+  mkdir -p ./docker/update
+
+  echo "Downloading latest version of E2E docker-compose package..."
+  gh auth status || gh auth login
+
+  # gh cannot overwrite existing files, therefore first download into separate dir. This way we still have the old copy
+  # in case the download fails
+  rm -rf ./docker/update/*
+  gh release download e2e-docker-compose --repo HSLdevcom/jore4-flux --dir ./docker/update
+  cp -R ./docker/update/* ./docker
 }
 
 start_all() {
