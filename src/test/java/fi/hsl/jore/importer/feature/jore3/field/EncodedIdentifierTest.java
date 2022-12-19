58fa3a332178f7a79d73866073d8f19c92a62a50
Tommi Leinamo <tommi.leinamo@vincit.fi>
2022-12-19 10:31:51 +0200

    Expose hidden variant from encoded identifier

--- src/test/java/fi/hsl/jore/importer/feature/jore3/field/EncodedIdentifierTest.java
+++ src/test/java/fi/hsl/jore/importer/feature/jore3/field/EncodedIdentifierTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static fi.hsl.jore.importer.feature.jore3.field.LegacyPublicTransportDestination.ESPOO;
@@ -28,66 +29,71 @@ public class EncodedIdentifierTest {
     private static final boolean IS_TEST = true;
     private static final boolean IS_PRODUCTION = false;
 
+    private static final Optional<Short> NO_HIDDEN_VARIANT = Optional.empty();
+    private static Optional<Short> hasHiddenVariant(int value) {
+        return Optional.of((short)value);
+    }
+
     private static Stream<Arguments> identifierAndPredicates() {
         return Stream.of(
                 // Tikkurila - Myyrmäki
-                arguments("0052B", LEGACY, "52B", IS_VARIANT, IS_PRODUCTION),
+                arguments("0052B", LEGACY, "52B", IS_VARIANT, IS_PRODUCTION, NO_HIDDEN_VARIANT),
                 // Raitiov. korv. bussi (this is a test line/route)
-                arguments("1000 X", HELSINKI, "X", IS_PRIMARY, IS_TEST),
+                arguments("1000 X", HELSINKI, "X", IS_PRIMARY, IS_TEST, NO_HIDDEN_VARIANT),
                 // Fredrikinkatu - Kampintori - Runeberginkatu (this is a test line/route)
-                arguments("1000A", HELSINKI, "A", IS_VARIANT, IS_TEST),
+                arguments("1000A", HELSINKI, "A", IS_VARIANT, IS_TEST, NO_HIDDEN_VARIANT),
                 // Rautatientori-Koskela (this is a test line/route)
-                arguments("1000S4", HELSINKI, "S", IS_VARIANT, IS_TEST),
+                arguments("1000S4", HELSINKI, "S", IS_VARIANT, IS_TEST, hasHiddenVariant(4)),
                 // Koskelan halli - Käpylä
-                arguments("1001 3", HELSINKI, "1", IS_VARIANT, IS_PRODUCTION),
+                arguments("1001 3", HELSINKI, "1", IS_VARIANT, IS_PRODUCTION, hasHiddenVariant(3)),
                 // Urheilutalo-Eira
-                arguments("1001A4", HELSINKI, "1A", IS_VARIANT, IS_PRODUCTION),
+                arguments("1001A4", HELSINKI, "1A", IS_VARIANT, IS_PRODUCTION, hasHiddenVariant(4)),
                 // Koskelan halli - Käpylä (this is a bus replacing the tram "3")
-                arguments("1003 X", HELSINKI, "3X", IS_PRIMARY, IS_PRODUCTION),
+                arguments("1003 X", HELSINKI, "3X", IS_PRIMARY, IS_PRODUCTION, NO_HIDDEN_VARIANT),
                 // suomenlinna
-                arguments("1019", HELSINKI, "19", IS_PRIMARY, IS_PRODUCTION),
+                arguments("1019", HELSINKI, "19", IS_PRIMARY, IS_PRODUCTION, NO_HIDDEN_VARIANT),
                 // Rautatientori-Koskela
-                arguments("1039", HELSINKI, "39", IS_PRIMARY, IS_PRODUCTION),
+                arguments("1039", HELSINKI, "39", IS_PRIMARY, IS_PRODUCTION, NO_HIDDEN_VARIANT),
                 // Kamppi-Lauttasaari-Kivenlahti
-                arguments("2147KT", ESPOO, "147KT", IS_VARIANT, IS_PRODUCTION),
+                arguments("2147KT", ESPOO, "147KT", IS_VARIANT, IS_PRODUCTION, NO_HIDDEN_VARIANT),
                 // Vanttila - Kauklahti - Lasilaakso
-                arguments("2166 1", ESPOO, "166", IS_VARIANT, IS_PRODUCTION),
+                arguments("2166 1", ESPOO, "166", IS_VARIANT, IS_PRODUCTION, hasHiddenVariant(1)),
                 // helsinki kerava (this is a legacy train line with no line letter)
-                arguments("3001", TRAIN_OR_METRO, "", IS_PRIMARY, IS_PRODUCTION),
+                arguments("3001", TRAIN_OR_METRO, "", IS_PRIMARY, IS_PRODUCTION, NO_HIDDEN_VARIANT),
                 // Helsinki-Riihimäki
-                arguments("3001D", TRAIN_OR_METRO, "D", IS_PRIMARY, IS_PRODUCTION),
+                arguments("3001D", TRAIN_OR_METRO, "D", IS_PRIMARY, IS_PRODUCTION, NO_HIDDEN_VARIANT),
                 // (reitillä ei otsikkoa)
-                arguments("3001I1", TRAIN_OR_METRO, "I", IS_VARIANT, IS_PRODUCTION),
+                arguments("3001I1", TRAIN_OR_METRO, "I", IS_VARIANT, IS_PRODUCTION, hasHiddenVariant(1)),
                 // Helsinki-Leppävaara
-                arguments("3002A", TRAIN_OR_METRO, "A", IS_PRIMARY, IS_PRODUCTION),
+                arguments("3002A", TRAIN_OR_METRO, "A", IS_PRIMARY, IS_PRODUCTION, NO_HIDDEN_VARIANT),
                 // A-juna
-                arguments("3002A3", TRAIN_OR_METRO, "A", IS_VARIANT, IS_PRODUCTION),
+                arguments("3002A3", TRAIN_OR_METRO, "A", IS_VARIANT, IS_PRODUCTION, hasHiddenVariant(3)),
                 // Tapiola - Mellunmäki
-                arguments("31M2", TRAIN_OR_METRO, "M2", IS_PRIMARY, IS_PRODUCTION),
+                arguments("31M2", TRAIN_OR_METRO, "M2", IS_PRIMARY, IS_PRODUCTION, NO_HIDDEN_VARIANT),
                 // Tapiola - Mellunmäki
-                arguments("31M2 4", TRAIN_OR_METRO, "M2", IS_VARIANT, IS_PRODUCTION),
+                arguments("31M2 4", TRAIN_OR_METRO, "M2", IS_VARIANT, IS_PRODUCTION, hasHiddenVariant(4)),
                 // Tapiola - Mellunmäki
-                arguments("31M2B4", TRAIN_OR_METRO, "M2B", IS_VARIANT, IS_PRODUCTION),
+                arguments("31M2B4", TRAIN_OR_METRO, "M2B", IS_VARIANT, IS_PRODUCTION, hasHiddenVariant(4)),
                 // Myllykylän th. - Koivikko - Katriinan sairaala - Riipilä
-                arguments("4001KM", VANTAA, "1KM", IS_VARIANT, IS_PRODUCTION),
+                arguments("4001KM", VANTAA, "1KM", IS_VARIANT, IS_PRODUCTION, NO_HIDDEN_VARIANT),
                 // Töölön kisahalli-Kivistö
-                arguments("4431 3", VANTAA, "431", IS_VARIANT, IS_PRODUCTION),
+                arguments("4431 3", VANTAA, "431", IS_VARIANT, IS_PRODUCTION, hasHiddenVariant(3)),
                 // Jupperi-Lähderanta-Espoon keskus
-                arguments("5530 3", REGIONAL_VANTAA_ESPOO, "530", IS_VARIANT, IS_PRODUCTION),
+                arguments("5530 3", REGIONAL_VANTAA_ESPOO, "530", IS_VARIANT, IS_PRODUCTION, hasHiddenVariant(3)),
                 // Kirkkonummi-Jorvas-Hirsala
-                arguments("6021SV", KIRKKONUMMI_SIUNTIO, "21SV", IS_VARIANT, IS_PRODUCTION),
+                arguments("6021SV", KIRKKONUMMI_SIUNTIO, "21SV", IS_VARIANT, IS_PRODUCTION, NO_HIDDEN_VARIANT),
                 // Kivenlahti-Masala-Kirkkonummi-Kantvik-Upinniemi
-                arguments("7177MB", U_BUS, "177MB", IS_VARIANT, IS_PRODUCTION),
+                arguments("7177MB", U_BUS, "177MB", IS_VARIANT, IS_PRODUCTION, NO_HIDDEN_VARIANT),
                 // Testi
-                arguments("8888", TESTING, "888", IS_PRIMARY, IS_TEST),
+                arguments("8888", TESTING, "888", IS_PRIMARY, IS_TEST, NO_HIDDEN_VARIANT),
                 // Kaskela-Kytömaa-Terveyskeskus
-                arguments("9008", TUUSULA_KERAVA_SIPOO, "8", IS_PRIMARY, IS_PRODUCTION),
+                arguments("9008", TUUSULA_KERAVA_SIPOO, "8", IS_PRIMARY, IS_PRODUCTION, NO_HIDDEN_VARIANT),
                 // Kaskela - Keravan asema
-                arguments("9008TT", TUUSULA_KERAVA_SIPOO, "8TT", IS_VARIANT, IS_PRODUCTION),
+                arguments("9008TT", TUUSULA_KERAVA_SIPOO, "8TT", IS_VARIANT, IS_PRODUCTION, NO_HIDDEN_VARIANT),
                 // Rautatientori - Jokivarsi - Nikkilä - Pornainen - Halkia
-                arguments("9787A4", TUUSULA_KERAVA_SIPOO, "787A", IS_VARIANT, IS_PRODUCTION),
+                arguments("9787A4", TUUSULA_KERAVA_SIPOO, "787A", IS_VARIANT, IS_PRODUCTION, hasHiddenVariant(4)),
                 // Rautatientori-Kuninkaanmäki-Nikkilä-Hinthaara-Porvoo
-                arguments("9788KV", TUUSULA_KERAVA_SIPOO, "788KV", IS_VARIANT, IS_PRODUCTION)
+                arguments("9788KV", TUUSULA_KERAVA_SIPOO, "788KV", IS_VARIANT, IS_PRODUCTION, NO_HIDDEN_VARIANT)
         );
     }
 
@@ -97,7 +103,8 @@ public class EncodedIdentifierTest {
                                      final LegacyPublicTransportDestination destination,
                                      final String displayId,
                                      final boolean isPrimary,
-                                     final boolean isTest) {
+                                     final boolean isTest,
+                                     final Optional<Short> hiddenVariant) {
         final LineId lineId = LineId.from(id);
 
         assertThat("destination is correct",
@@ -115,5 +122,9 @@ public class EncodedIdentifierTest {
         assertThat("test flag is correct",
                    lineId.test(),
                    is(isTest));
+
+        assertThat("hidden variant is correct",
+                   lineId.hiddenVariantValue(),
+                   is(hiddenVariant));
     }
 }
