51084837850c429128b0c28bbf027e225d5b0990
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-01-27 10:24:19 +0200

    Split test data inserts to multiple lines

--- src/test/resources/sql/destination/populate_routes_staging.sql
+++ src/test/resources/sql/destination/populate_routes_staging.sql
@@ -1,4 +1,10 @@
-INSERT INTO network.network_routes_staging (network_line_ext_id, network_route_ext_id, network_route_number, network_route_hidden_variant, network_route_name)
+INSERT INTO network.network_routes_staging (
+        network_line_ext_id,
+        network_route_ext_id,
+        network_route_number,
+        network_route_hidden_variant,
+        network_route_name
+)
 VALUES (
         '1001',
         '1001 3',
