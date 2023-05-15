bcc54e820add159d13f65d1bab621bc8cbac234f
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-05-15 13:33:39 +0300

    Refactor truncating jore4 tables.

--- src/test/resources/sql/jore4/drop_tables.sql
+++ src/test/resources/sql/jore4/drop_tables.sql
@@ -1,10 +1,25 @@
-BEGIN; TRUNCATE TABLE service_pattern.vehicle_mode_on_scheduled_stop_point CASCADE; COMMIT;
-BEGIN; TRUNCATE TABLE route.infrastructure_link_along_route CASCADE; COMMIT;
-BEGIN; TRUNCATE TABLE infrastructure_network.vehicle_submode_on_infrastructure_link CASCADE; COMMIT;
-BEGIN; TRUNCATE TABLE infrastructure_network.infrastructure_link CASCADE; COMMIT;
-BEGIN; TRUNCATE TABLE service_pattern.scheduled_stop_point CASCADE; COMMIT;
-BEGIN; TRUNCATE TABLE route.line CASCADE; COMMIT;
-BEGIN; TRUNCATE TABLE route.route CASCADE; COMMIT;
-BEGIN; TRUNCATE TABLE timing_pattern.timing_place CASCADE; COMMIT;
-BEGIN; TRUNCATE TABLE journey_pattern.journey_pattern CASCADE; COMMIT;
-BEGIN; TRUNCATE TABLE journey_pattern.scheduled_stop_point_in_journey_pattern CASCADE; COMMIT;
+BEGIN;
+    -- Changes the mode of all deferrable constraints.
+    -- Done to prevent pending trigger events aborting transaction.
+    -- Affects only current transaction.
+    SET CONSTRAINTS ALL IMMEDIATE;
+
+    -- Table truncation is done in a single statement to avoid cascading.
+    -- Tables are ordered based on foreign key references (referred last).
+    TRUNCATE TABLE
+        journey_pattern.scheduled_stop_point_in_journey_pattern,
+        journey_pattern.journey_pattern,
+
+        route.infrastructure_link_along_route,
+        route.route,
+        route.line,
+
+        service_pattern.vehicle_mode_on_scheduled_stop_point,
+        service_pattern.scheduled_stop_point,
+        service_pattern.scheduled_stop_point_invariant,
+
+        timing_pattern.timing_place,
+
+        infrastructure_network.vehicle_submode_on_infrastructure_link,
+        infrastructure_network.infrastructure_link;
+COMMIT;
