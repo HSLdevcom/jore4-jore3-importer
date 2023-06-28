a954f8c1ba148a149e06ff857822cb6b70a3014f
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-06-28 19:21:37 +0300

    Replace name `hastusPlaceId` with `placeExternalId` in many places as this more closely follows the naming convention used in the code base.

--- src/main/java/fi/hsl/jore/importer/feature/batch/scheduled_stop_point/ScheduledStopPointImportProcessor.java
+++ src/main/java/fi/hsl/jore/importer/feature/batch/scheduled_stop_point/ScheduledStopPointImportProcessor.java
@@ -25,7 +25,7 @@ public class ScheduledStopPointImportProcessor implements ItemProcessor<JrSchedu
                         .with(JoreLocaleUtil.FINNISH, input.nameFinnish())
                         .with(JoreLocaleUtil.SWEDISH, input.nameSwedish()),
                 constructShortId(input),
-                input.hastusPlaceId().filter(StringUtils::isNotBlank), // trim whitespace entries to empty (null)
+                input.placeExternalId().filter(StringUtils::isNotBlank), // trim whitespace entries to empty (null)
                 input.usageInRoutes()
         );
     }
