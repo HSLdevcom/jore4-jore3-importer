141775dd2dc3f80f77622635894f73e284cf72e4
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-08-01 16:16:08 +0300

    Export timing place names to Jore4 database.

--- src/test/java/fi/hsl/jore/importer/feature/jore4/repository/Jore4TimingPlaceRepositoryTest.java
+++ src/test/java/fi/hsl/jore/importer/feature/jore4/repository/Jore4TimingPlaceRepositoryTest.java
@@ -1,6 +1,8 @@
 package fi.hsl.jore.importer.feature.jore4.repository;
 
 import fi.hsl.jore.importer.IntTest;
+import fi.hsl.jore.importer.feature.common.dto.field.MultilingualString;
+import fi.hsl.jore.importer.feature.jore3.util.JoreLocaleUtil;
 import fi.hsl.jore.importer.feature.jore4.entity.Jore4TimingPlace;
 import org.assertj.db.type.Table;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +17,7 @@ import javax.sql.DataSource;
 import java.util.List;
 import java.util.UUID;
 
+import static fi.hsl.jore.importer.TestJsonUtil.equalJson;
 import static fi.hsl.jore.jore4.jooq.timing_pattern.Tables.TIMING_PLACE;
 import static org.assertj.db.api.Assertions.assertThat;
 
@@ -23,6 +26,9 @@ public class Jore4TimingPlaceRepositoryTest {
 
     private static final UUID TIMING_PLACE_ID = UUID.fromString("b5f56d68-c4cf-11ed-afa1-0242ac120002");
     private static final String TIMING_PLACE_LABEL = "1ELIEL";
+    private static final String TIMING_PLACE_NAME = "Elielinaukio";
+
+    private static final String EXPECTED_RESULTING_TIMING_PLACE_NAME = "{\"fi_FI\":\"" + TIMING_PLACE_NAME + "\"}";
 
     private final IJore4TimingPlaceRepository repository;
     private final Table targetTable;
@@ -45,7 +51,10 @@ public class Jore4TimingPlaceRepositoryTest {
     )
     class InsertLineIntoDatabase {
 
-        private final Jore4TimingPlace INPUT = Jore4TimingPlace.of(TIMING_PLACE_ID, TIMING_PLACE_LABEL);
+        private final Jore4TimingPlace INPUT = Jore4TimingPlace.of(
+                TIMING_PLACE_ID,
+                TIMING_PLACE_LABEL,
+                MultilingualString.empty().with(JoreLocaleUtil.FINNISH, TIMING_PLACE_NAME));
 
         @Test
         @DisplayName("Should insert one timing place into the database")
@@ -74,6 +83,17 @@ public class Jore4TimingPlaceRepositoryTest {
                     .row()
                     .value(TIMING_PLACE.LABEL.getName())
                     .isEqualTo(TIMING_PLACE_LABEL);
+        }
+
+        @Test
+        @DisplayName("Should insert the correct name into the database")
+        void shouldInsertCorrectNameIntoDatabase() {
+            repository.insert(List.of(INPUT));
+
+            assertThat(targetTable)
+                    .row()
+                    .value(TIMING_PLACE.DESCRIPTION.getName())
+                    .is(equalJson(EXPECTED_RESULTING_TIMING_PLACE_NAME));
         }
     }
 }
