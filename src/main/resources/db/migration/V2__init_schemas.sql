7b6098c8ec41534df6ef6d7f1d133571a6ae5d18
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-02-21 14:47:56 +0200

    Create PostgreSQL extensions conditionally.
    This is because in Azure PostgreSQL Flexible Server v15, ordinary users (without
    the azure_pg_admin role) cannot create extensions. These extensions need to be
    created before deploying this service into the cloud.
    
    Add the creation of the PostGIS extension to the migrations for consistency and
    because it may not be available in all database environments.
    
    When using PostGIS Docker image as a base for database, the postgis extension
    already exists.

--- src/main/resources/db/migration/V2__init_schemas.sql
+++ src/main/resources/db/migration/V2__init_schemas.sql
@@ -2,6 +2,49 @@ CREATE SCHEMA infrastructure_network;
 
 CREATE SCHEMA network;
 
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
+-- Create extension conditionally. This is because in Azure PostgreSQL Flexible
+-- Server v15, ordinary users (without the azure_pg_admin role) cannot create
+-- extensions. These extensions need to be created before deploying this service
+-- into the cloud.
 
-CREATE EXTENSION IF NOT EXISTS btree_gist;
+-- Conditionally create the btree_gist extension.
+DO $$
+DECLARE
+    ext_exists BOOLEAN;
+BEGIN
+    -- Check if btree_gist extension exists.
+    SELECT EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'btree_gist') INTO ext_exists;
+
+    -- Create extension only if it doesn't exist.
+    IF NOT ext_exists THEN
+        CREATE EXTENSION IF NOT EXISTS btree_gist SCHEMA public;
+    END IF;
+END $$;
+
+-- Conditionally create the pgcrypto extension.
+DO $$
+DECLARE
+    ext_exists BOOLEAN;
+BEGIN
+    -- Check if pgcrypto extension already exists.
+    SELECT EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'pgcrypto') INTO ext_exists;
+
+    -- Create extension only if it doesn't exist.
+    IF NOT ext_exists THEN
+        CREATE EXTENSION IF NOT EXISTS pgcrypto SCHEMA public;
+    END IF;
+END $$;
+
+-- Conditionally create the postgis extension.
+DO $$
+DECLARE
+    ext_exists BOOLEAN;
+BEGIN
+    -- Check if postgis extension already exists.
+    SELECT EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'postgis') INTO ext_exists;
+
+    -- Create extension only if it doesn't exist.
+    IF NOT ext_exists THEN
+        CREATE EXTENSION IF NOT EXISTS postgis SCHEMA public;
+    END IF;
+END $$;
