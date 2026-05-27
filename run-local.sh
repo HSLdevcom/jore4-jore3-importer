0e39d4b9d7d503a2f365c2db3da7cca7dfd332ba
Janne Bergman <janne.bergman@vincit.fi>
2026-05-27 13:48:11 +0300

    TLS settings for old mssql server in azure

--- run-local.sh
+++ run-local.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 JORE_IMPORTER_MIGRATE=true \
-  mvn clean spring-boot:run -Dspring-boot.run.jvmArguments="-Xmx4096m"
+  mvn clean spring-boot:run -Dspring-boot.run.jvmArguments="-Xmx4096m -Djdk.tls.client.protocols=TLSv1,TLSv1.1,TLSv1.2 -Djava.security.properties=java.security.override"
 
